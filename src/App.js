3ec584a99676616759f4650c2822e1e5e623dfd4
GauravShreenet <gthakuri656@gmail.com>
2023-10-06 20:52:45 +1100

    added bootstrap fontawesome

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="">
+    <div className="wrapper">
+      <div className="container">
+        <div className="row">
+          <div className="col text-center mt-5">
+            <h1>Know Your Comic Character</h1>
+          </div>
+        </div>
+        <hr />
+
+        
+      </div>
     </div>
   );
 }
