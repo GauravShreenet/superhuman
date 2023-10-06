7c2ff6df74f412dd2bb1a2bd9a5b7a01e4caa333
GauravShreenet <gthakuri656@gmail.com>
2023-10-06 23:48:16 +1100

    testing the fetch

--- src/component/SearchForm.js
+++ src/component/SearchForm.js
@@ -1,8 +1,27 @@
 
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { CardLayout } from './CardLayout'
+import { fetchCharacter } from '../utils/axiosHelper';
 
 export const SearchForm = () => {
+    const [character, setCharacter] = useState({});
+    const strRef = useRef("");
+    const [error, setError] = useState("")
+
+const handleOnSubmit = async(e) => {
+    e.preventDefault();
+    setCharacter({});
+    setError("");
+    const str = strRef.current.value;
+
+    const data = await fetchCharacter(str);
+
+    if (data.Response === "True") {
+        setCharacter(data);
+    }else{
+        setError(data.Error)
+    }
+}
 
   return (
     <div className="shadow rounded-2 p-5 mt-5">
@@ -10,7 +29,7 @@ export const SearchForm = () => {
             <div className="col">
                 <h3>Super Hero</h3>
             </div>
-            <form>
+            <form onSubmit={handleOnSubmit}>
                 <div className="row g-3">
                     <div className="col-md-9">
                         <input type="text" placeholder="Enter the Name..." id='search' className='form-control'/>
@@ -23,7 +42,7 @@ export const SearchForm = () => {
                 </div>
             </form>
             <div className="col-md mt-3 d-flex justify-content-center flex-wrap gap-5">
-                <CardLayout />
+                <CardLayout character={character}/>
                 
             </div>
         </div>
--- src/utils/axiosHelper.js
+++ src/utils/axiosHelper.js
@@ -6,8 +6,8 @@ const accessToken = "2039640043063608"
 export const fetchCharacter = async (name) => {
     try {
         const response = await axios.get(`${apiUrl}${accessToken}/search/${name}`);
-        console.log(response.result)
-        return response.result;  
+        console.log(response.results)
+        return response.results;  
     }catch(error){
         console.log(error)
     }
