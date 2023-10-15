046e4671e4fac01453265e17f14b9c13126d14b9
Gaurav Shreenet <gthakuri656@gmail.com>
2023-10-15 22:57:01 +1100

    if there is no data it will show not provided rather than blank space

--- src/component/DetailComp.js
+++ src/component/DetailComp.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
 export const DetailComp = ({ characterList, category }) => {
+
+  const renderValue = (value) => {
+    if(value === "null" || value === "-" || value === "") {
+      return "Not Provided"
+    }
+    return value;
+  }
+
   return (
     <div className="card mb-3" style={{ width: "200vh" }}>
       <div className="row g-0">
@@ -11,30 +19,30 @@ export const DetailComp = ({ characterList, category }) => {
           <div className="card-body">
             {category === 'detail' && (
               <>
-                <h5 className="card-title mt-5"><strong>Full Name: </strong>{characterList?.character?.biography['full-name']}</h5>
-                <h5 className="card-title mt-3"><strong>Eye-Color: </strong>{characterList?.character?.appearance['eye-color']}</h5>
-                <h5 className="card-title mt-3"><strong>Gender: </strong>{characterList?.character?.appearance['gender']}</h5>
-                <h5 className="card-title mt-3"><strong>Height: </strong>{characterList?.character?.appearance?.height[0]}</h5>
-                <h5 className="card-title mt-3"><strong>First-Appearance: </strong>{characterList?.character?.biography['first-appearance']}</h5>
-                <p className="card-text mt-5"><small className="text-body-secondary">{characterList?.character?.biography?.publisher}</small></p>
+                <h5 className="card-title mt-5"><strong>Full Name: </strong>{renderValue(characterList?.character?.biography['full-name'])}</h5>
+                <h5 className="card-title mt-3"><strong>Eye-Color: </strong>{renderValue(characterList?.character?.appearance['eye-color'])}</h5>
+                <h5 className="card-title mt-3"><strong>Gender: </strong>{renderValue(characterList?.character?.appearance['gender'])}</h5>
+                <h5 className="card-title mt-3"><strong>Height: </strong>{renderValue(characterList?.character?.appearance?.height[0])}</h5>
+                <h5 className="card-title mt-3"><strong>First-Appearance: </strong>{renderValue(characterList?.character?.biography['first-appearance'])}</h5>
+                <p className="card-text mt-5"><small className="text-body-secondary">{renderValue(characterList?.character?.biography?.publisher)}</small></p>
               </>
             )}
             {category === 'connections' && (
               <>
-                <h5 className="card-title mt-5"><strong>Group-affiliation: </strong>{characterList?.character?.connections['group-affiliation']}</h5>
-                <h5 className="card-title mt-3"><strong>Relatives: </strong>{characterList?.character?.connections['relatives']}</h5>
-                <p className="card-text mt-5"><small className="text-body-secondary">{characterList?.character?.biography?.publisher}</small></p>
+                <h5 className="card-title mt-5"><strong>Group-affiliation: </strong>{renderValue(characterList?.character?.connections['group-affiliation'])}</h5>
+                <h5 className="card-title mt-3"><strong>Relatives: </strong>{renderValue(characterList?.character?.connections['relatives'])}</h5>
+                <p className="card-text mt-5"><small className="text-body-secondary">{renderValue(characterList?.character?.biography?.publisher)}</small></p>
               </>
             )}
             {category === 'powerStats' && (
               <>
-                <h5 className="card-title mt-5"><strong>Combat: </strong>{characterList?.character?.powerstats['combat']}</h5>
-                <h5 className="card-title mt-3"><strong>Durability: </strong>{characterList?.character?.powerstats['durability']}</h5>
-                <h5 className="card-title mt-3"><strong>Intelligence: </strong>{characterList?.character?.powerstats['intelligence']}</h5>
-                <h5 className="card-title mt-3"><strong>Power: </strong>{characterList?.character?.powerstats['power']}</h5>
-                <h5 className="card-title mt-3"><strong>Speed: </strong>{characterList?.character?.powerstats['speed']}</h5>
-                <h5 className="card-title mt-3"><strong>Strength: </strong>{characterList?.character?.powerstats['strength']}</h5>
-                <p className="card-text mt-5"><small className="text-body-secondary">{characterList?.character?.biography?.publisher}</small></p>
+                <h5 className="card-title mt-5"><strong>Combat: </strong>{renderValue(characterList?.character?.powerstats['combat'])}</h5>
+                <h5 className="card-title mt-3"><strong>Durability: </strong>{renderValue(characterList?.character?.powerstats['durability'])}</h5>
+                <h5 className="card-title mt-3"><strong>Intelligence: </strong>{renderValue(characterList?.character?.powerstats['intelligence'])}</h5>
+                <h5 className="card-title mt-3"><strong>Power: </strong>{renderValue(characterList?.character?.powerstats['power'])}</h5>
+                <h5 className="card-title mt-3"><strong>Speed: </strong>{renderValue(characterList?.character?.powerstats['speed'])}</h5>
+                <h5 className="card-title mt-3"><strong>Strength: </strong>{renderValue(characterList?.character?.powerstats['strength'])}</h5>
+                <p className="card-text mt-5"><small className="text-body-secondary">{renderValue(characterList?.character?.biography?.publisher)}</small></p>
               </>
             )}
 
